25c163f8ee0d6c72cf1840f31b5aa6ae845ca343
hahaman110 <lil>
2018-11-26 14:30:01 +0800

    idea test

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * ZYD 2018/11/26
+ */
+public class Test {
+    public static String key;
+
+    static {
+        key = "hahahahahah";
+    }
+}
---
+++ src/Test2.java
@@ -0,0 +1,9 @@
+/**
+ * ZYD 2018/11/26
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println(new Test().key);
+    }
+}
